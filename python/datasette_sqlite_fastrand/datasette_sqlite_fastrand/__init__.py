5589c3153a72973f9189a8b1f3b0a471058ba91b
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-02-21 15:10:52 -0800

    make version

--- python/datasette_sqlite_fastrand/datasette_sqlite_fastrand/__init__.py
+++ python/datasette_sqlite_fastrand/datasette_sqlite_fastrand/__init__.py
@@ -1,6 +1,8 @@
 from datasette import hookimpl
 import sqlite_fastrand
 
+from datasette_sqlite_fastrand.version import __version_info__, __version__ 
+
 @hookimpl
 def prepare_connection(conn):
     conn.enable_load_extension(True)
